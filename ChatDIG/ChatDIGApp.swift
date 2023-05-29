e3f399ca95ec931ccf60bc6f22f051003f0d995d
corazonnn <zero7364@icloud.com>
2023-05-29 18:42:53 +0900

    Initial Commit

---
+++ ChatDIG/ChatDIGApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatDIGApp.swift
+//  ChatDIG
+//
+//  Created by 相川佑也 on 2023/05/29.
+//
+
+import SwiftUI
+
+@main
+struct ChatDIGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
